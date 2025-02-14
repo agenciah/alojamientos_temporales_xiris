eebef531564e28e200114d3f7ea44f62f50efd04
Lijazilla <alejandro-1986-jf@hotmail.com>
2025-02-13 19:38:46 -0600

    cambios en package json y vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  case: "/aviso_mudanza_xiris"
+  case: "/residenza_condominio_xiris_aviso_de_mudanza"
 })
